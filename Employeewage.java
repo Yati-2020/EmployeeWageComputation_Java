8ec3929873af64a4a4d9998c7df3d2d1e3be4aac
yatiP <yati.patil17@gmail.com>
2020-12-24 14:50:15 +0530

    Employee Program to calculate Daily Wage

--- Employeewage.java
+++ Employeewage.java
@@ -3,5 +3,26 @@ public class Employeewage
         public static void main(String[] args)
         {
                 System.out.println("Welcome to Employee Wage Computation Program");
+		int fullTime=1;
+                int empHr=0;
+                int empRatePerHr=20;
+                int salary=0;
+                int Result=(int)(Math.random()*10)%2;
+
+                System.out.println("Result="+ Result);
+
+        if (Result == fullTime )
+                {
+                        empHr=8;
+                }
+
+        else
+                {
+                        salary=0;
+                }
+
+        salary=empHr*empRatePerHr;
+        System.out.println("Salary= " + salary);
+
         }
 }
