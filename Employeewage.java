f4479d869745a593591da87bbf89bf72e0ebc367
yatiP <yati.patil17@gmail.com>
2020-12-24 15:18:15 +0530

    Program for Part time Employee & Wage

--- Employeewage.java
+++ Employeewage.java
@@ -3,5 +3,31 @@ public class Employeewage
         public static void main(String[] args)
         {
                 System.out.println("Welcome to Employee Wage Computation Program");
+		int fullTime=1;
+                int partTime=2;
+                int empHr=0;
+                int empRatePerHr=20;
+                int salary=0;
+                int Result=(int)(Math.random()*10)%3;
+
+                System.out.println("Result="+ Result);
+
+        if (Result == fullTime )
+                {
+                        empHr=8;
+                }
+        else if(Result == partTime)
+                {
+                        empHr=4;
+                }
+        else
+                {
+                        salary=0;
+                }
+
+        salary=empHr*empRatePerHr;
+        System.out.println("Salary= " + salary);
+
+
         }
 }
