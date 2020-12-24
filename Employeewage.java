779d613c7c0dc42296ca5d9dd6e7e2a07c41f0f3
yatiP <yati.patil17@gmail.com>
2020-12-24 15:34:01 +0530

    Program for Part time Employee & Wage using Switch case

--- Employeewage.java
+++ Employeewage.java
@@ -1,7 +1,33 @@
 public class Employeewage
 {
+	public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
+        public static final int EMP_RATE_PER_HR=20;
+
+
         public static void main(String[] args)
         {
-                System.out.println("Welcome to Employee Wage Computation Program");
+
+                int empHr=0;   
+                int salary=0;
+                int Result=(int)Math.floor(Math.random()*10)%3;
+
+                switch(Result)
+                {
+                        case FULL_TIME:
+                        empHr=8;
+                        break;
+
+                        case PART_TIME:
+                        empHr=4;
+                        break;
+
+                        default:
+                        empHr=0;
+                }
+
+                salary=empHr*EMP_RATE_PER_HR;
+                System.out.println("Employee Wage: "+salary);
+
         }
 }
